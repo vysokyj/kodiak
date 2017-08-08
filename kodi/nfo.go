ab04d3e8fa15beeab108765f6070d0ed8bb2f581
Jiří Vysoký <jiri.vysoky@ys.cz>
2017-08-08 17:32:12 +0200

    Implemented NFO

--- kodi/nfo.go
+++ kodi/nfo.go
@@ -1,36 +1,89 @@
 package kodi
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+)
 
+// Movie is root xml element
 type Movie struct {
-	XMLName       xml.Name `xml:"movie"`
-	Title         string   `xml:"title"`
-	OriginalTitle string   `xml:"originaltitle"`
-	Rating        float32  `xml:"rating"`
-	Votes         int      `xml:"votes"`
-	Top250        int      `xml:"top250"`
-	Year          int      `xml:"year"`
-	Plot          string   `xml:"plot"`
-	Outline       string   `xml:"outline"`
-	Tagline       string   `xml:"tagline"`
-	Runtime       int      `xml:"runtime"`
-	MPAA          string   `xml:"mpaa"`
-	PlayCount     int      `xml:"playcount"`
-	ID            string   `xml:"id"`
-	TmdbID        int64    `xml:"tmdbid"`
-	Set           string   `xml:"set"`
-	SortTitle     string   `xml:"sorttitle"`
-	Trailer       string   `xml:"trailer"`
-	Watched       bool     `xml:"watched"`
-	Credits       string   `xml:"credits"`
-	Director      string   `xml:"director"`
-	Studio        []string `xml:"studio"`
-	Genre         []string `xml:"genre"`
-	Country       []string `xml:"country"`
+	XMLName       xml.Name    `xml:"movie"`
+	Title         string      `xml:"title"`
+	OriginalTitle string      `xml:"originaltitle"`
+	Rating        float32     `xml:"rating"`
+	Votes         int         `xml:"votes"`
+	Top250        int         `xml:"top250"`
+	Year          int         `xml:"year"`
+	Plot          string      `xml:"plot"`
+	Outline       string      `xml:"outline"`
+	Tagline       string      `xml:"tagline"`
+	Runtime       int         `xml:"runtime"`
+	MPAA          string      `xml:"mpaa"`
+	PlayCount     int         `xml:"playcount"`
+	ID            string      `xml:"id"`
+	TmdbID        int64       `xml:"tmdbid"`
+	Set           string      `xml:"set"`
+	SortTitle     string      `xml:"sorttitle"`
+	Trailer       string      `xml:"trailer"`
+	Watched       bool        `xml:"watched"`
+	Credits       []string    `xml:"credits"`
+	Directors     []string    `xml:"director"`
+	Studios       []string    `xml:"studio"`
+	Genres        []string    `xml:"genre"`
+	Countries     []string    `xml:"country"`
+	Thumbnails    []Thumbnail `xml:"thumb"`
+	Fanart        Fanart      `xml:"fanart"`
+	Actors        []*Actor    `xml:"actor"`
+	FileInfo      FileInfo    `xml:"fileinfo"`
 }
 
-type Thumb struct {
+// Actor is nested element in movie
+type Actor struct {
+	XMLName   xml.Name  `xml:"actor"`
+	Name      string    `xml:"name"`
+	Role      string    `xml:"role"`
+	Thumbnail Thumbnail `xml:"thumb"`
+}
+
+// Fanart is nested element in movies
+type Fanart struct {
+	XMLName    xml.Name    `xml:"fanart"`
+	Thumbnails []Thumbnail `xml:"thumb"`
+}
+
+// Thumbnail is nested element in movie and others
+type Thumbnail struct {
 	XMLName xml.Name `xml:"thumb"`
+	Preview string   `xml:"preview,attr,omitempty"`
+	Path    string   `xml:",chardata"`
 }
 
-type
+// FileInfo holds file info
+type FileInfo struct {
+	XMLName       xml.Name      `xml:"fileinfo"`
+	StreamDetails StreamDetails `xml:"streamdetails"`
+}
+
+// StreamDetails holds stream details
+type StreamDetails struct {
+	XMLName xml.Name `xml:"streamdetails"`
+	Video   Video    `xml:"video"`
+	Audio   Audio    `xml:"audio"`
+}
+
+// Video holds vido data
+type Video struct {
+	XMLName           xml.Name `xml:"video"`
+	Aspect            float32  `xml:"aspect"`
+	Codec             string   `xml:"codec"`
+	DurationInSeconds int64    `xml:"durationinseconds"`
+	Height            int      `xml:"height"`
+	Width             int      `xml:"width"`
+	Scantype          string   `xml:"scantype"`
+}
+
+// Audio holds audio data
+type Audio struct {
+	XMLName  xml.Name `xml:"audio"`
+	Channels int      `xml:"channels"`
+	Codec    string   `xml:"codec"`
+}
---
+++ kodi/nfo_test.go
@@ -0,0 +1,30 @@
+package kodi
+
+import (
+	"encoding/xml"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestUnmarshallMarshall(t *testing.T) {
+	xmlFile, err := os.Open("movie.nfo")
+	if err != nil {
+		panic(err)
+	}
+	defer xmlFile.Close()
+	b, _ := ioutil.ReadAll(xmlFile)
+
+	movie := &Movie{}
+	err = xml.Unmarshal(b, &movie)
+	if err != nil {
+		panic(err)
+	}
+	output, err := xml.MarshalIndent(movie, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	os.Stdout.Write([]byte(xml.Header))
+	os.Stdout.Write(output)
+	os.Stdout.WriteString("\n")
+}
